2b99e669d0f98411556bd3a5e32138a284a008d6
Klondike Taylor <klondiketaylor@gmail.com>
2021-01-16 15:53:39 -0700

    I cant pull so let's see if this will work

--- bot.py
+++ bot.py
@@ -18,7 +18,6 @@ async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-        
     print(
         f'{client.user} is connected to the following guild: \n'
         f'{guild.name}(id: {guild.id})'
