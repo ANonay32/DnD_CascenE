3ed6a3339747c246e4e941e07ed733e54d176b2c
Noah Batiuk <51248438+CryingOutCloud@users.noreply.github.com>
2021-01-16 23:43:58 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -220,7 +220,7 @@ async def move(ctx, name, newx: int, newy: int):
         await ctx.channel.send("This player does not exist. Check your spelling or use &listplayers.")
         return
 
-    listMap[old[2] * gwidth + old[1] * 2] = " "
+    listMap[old[2] * gwidth + old[1]] = " "
     if len(name) > 1:
         listMap[newy * gwidth + newx] = name[0]
     else:
