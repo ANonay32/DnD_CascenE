bd411815da7ce207ec9b8ec1578733af5737f843
Noah Batiuk <51248438+CryingOutCloud@users.noreply.github.com>
2021-01-16 23:04:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+<<<<<<< HEAD
+
+=======
+"""
+Created on Sat Jan 16 22:26:21 2021
+
+@author: lucas, Arthur, Noah, Klondike
+"""
+>>>>>>> dc4b8edf73eb58e7cbeeae07c7bfea1eaf6ad036
+
 
 import nest_asyncio
 nest_asyncio.apply()
@@ -60,6 +71,7 @@ async def game_map(ctx, width: int, height: int):
             gmap += "\n"
         await ctx.channel.send("```" + gmap + "```")
 
+
 @bot.command()
 async def build(ctx, tlx: int, tly: int, brx: int, bry: int):
 
@@ -69,21 +81,36 @@ async def build(ctx, tlx: int, tly: int, brx: int, bry: int):
 
     chArray = list(gmap)
 
+    if tlx>brx:
+        temp = tlx
+        tlx = brx
+        brx = temp
+        
+    if tly>bry:
+        temp = tly
+        tly = bry
+        bry = temp
+
     for k in range(abs(tly - bry)):
         for l in range(abs(tlx - brx)*2):
             if l == 0 or l == abs(tlx - brx)*2 - 1:
-                chArray[((tly + k)*gwidth + l + tlx*2 + 1)] = "|"
+                chArray[((tly + k)*gwidth + l + tlx*2)] = "‖"
             elif k == abs(tly - bry) - 1:
-                chArray[((tly + k)*gwidth + l + tlx*2 + 1)] = "_"
+                chArray[((tly + k)*gwidth + l + tlx*2)] = "‗"
             elif k == 0:
-                chArray[((tly + k)*gwidth + l + tlx*2 + 1)] = "‾"
+                chArray[((tly + k)*gwidth + l + tlx*2)] = "˭"
 
             #await ctx.channel.send(str((tly + k)*gwidth) + " " + str(l) + " " + str(tlx))
 
     gmap = "".join(chArray)
 
     await ctx.channel.send("```" + gmap + "```")
+<<<<<<< Updated upstream
 
+=======
+    
+    
+>>>>>>> Stashed changes
 @bot.command()
 async def build_spec(ctx, tlx: int, tly: int, brx: int, bry: int, char: str):
 
@@ -109,14 +136,13 @@ async def build_spec(ctx, tlx: int, tly: int, brx: int, bry: int, char: str):
     await ctx.channel.send("```" + gmap + "```")
 
 @bot.command()
-<<<<<<< HEAD
 async def save_as(ctx, filename: str):
 	try:
 		global gmap
 		global gwidth
 		global gheight
 		f = open("_".join(["userFile",ctx.user,filename]), "w")
-		f.write("\t".join([gwidth, gheight, gmap]))
+		f.write("\t".join([str(gwidth), str(gheight), gmap]))
 		f.close()
 		await ctx.channel.send("```File Written```")
 	except Exception as e:
@@ -130,24 +156,29 @@ async def load_map(ctx, filename: str):
 		global gheight
 		f = open("_".join(["userFile",ctx.user,filename]), "r")
 		temp = f.read()
-		gwidth, gheight, gmap = temp.split("\t")
+		tgwidth, tgheight, gmap = temp.split("\t")
+		gwidth = int(tgwidth)
+		gheight = int(tgheight)
 	except Exception as e:
-		await ctx.channel.send(f"```Error when reading file '{filename}' for {ctx.user}:\n {str(e)}"[0:1996]+"```")
-
-
+		await ctx.channel.send(f"```Error when reading file '{filename}':\n {str(e)}"[0:1996]+"```")
 
 @bot.command()
-<<<<<<< HEAD
+async def listplayers(ctx):
+    for player in players:
+        await ctx.channel.send(player[0])
+
+@bot.command()
 async def print(ctx, arg):
     await ctx.channel.send(arg)
-=======
-=======
+
+@bot.command()
 async def add_player(ctx, name, xpos: int, ypos: int):
     global players
     global gmap
     global gwidth
     global gheight
 
+    xpos = xpos*2
     valid = True
     if xpos < 0 or xpos > gwidth - 1 or ypos < 0 or ypos > gheight - 1:
         await ctx.channel.send("Your character would be out of bounds at these coordinates")
@@ -166,8 +197,144 @@ async def add_player(ctx, name, xpos: int, ypos: int):
         await ctx.channel.send("```" + gmap + "```")
 
 @bot.command()
->>>>>>> cad1c5dd70b786555027484e7d38e469afb695de
+async def move(ctx, name, newx: int, newy: int):
+    
+    newx = newx*2
+    global gmap
+    listMap = list(gmap)
+    old = None
+    
+    valid = True
+    if newx < 0 or newx > gwidth - 1 or newy < 0 or newy > gheight - 1:
+        await ctx.channel.send("Your character would be out of bounds at these coordinates")
+        valid = False
+    if listMap[newy * gwidth + newx] != " ":
+        await ctx.channel.send("Your character would be inside a wall or something at these coordinates")
+        valid = False
+        
+    for player in players:
+        if player[0] == name:
+            old = player
+           
+        if old == None:
+                await ctx.channel.send("This player does not exist. Check your spelling or use &players.")
+                return
+        
+    listMap[old[2] * gwidth + old[1]] = " "
+    if len(name) > 1:
+        listMap[newy * gwidth + newx] = name[0]
+    else:
+        listMap[newy * gwidth + newx] = name
+    if valid:
+        players.append((name, newx, newy))
+        gmap = "".join(listMap)
+        await ctx.channel.send("```" + gmap + "```")
+<<<<<<< HEAD
+<<<<<<< HEAD
+        
+@bot.command()
+async def line(ctx, x0: int, y0: int, x1: int, y1: int, char: str):
+    
+    global gwidth
+    global gheight
+    global gmap
+
+
+    #start
+
+    points = [];
+    dx = x1 - x0
+    dy = y1 - y0
+    N =  max(abs(dx), abs(dy)*2);   #may move *2 to l in points loop
+    
+    for step in range(N+1):
+        
+        t = 0
+        if (N == 0):
+            t = 0.0
+        else:
+            t = step / N
+        
+        vx = round(x0 + t * dx)
+        vy = round(y0 + t * dy)
+        
+        pxy = (vx, vy)
+        
+        points.append(pxy)
+        
+
+    chArray = list(gmap)
+    
+    
+    for pr in points:
+        chArray[(pr[1]*gwidth + pr[0]*2 + 1)] = char
+    
+    gmap = "".join(chArray)
+    
+    await ctx.channel.send("```" + gmap + "```")
+
+
+<<<<<<< Updated upstream
+=======
+
+
+@bot.command()
+async def line(ctx, x0: int, y0: int, x1: int, y1: int, char: str):
+    
+    global gwidth
+    global gheight
+    global gmap
+    
+
+    #start
+
+    points = [];
+    dx = x1 - x0
+    dy = y1 - y0
+    N =  max(abs(dx), abs(dy));
+    
+    for step in range(N+1):
+        
+        t = 0
+        if (N == 0):
+            t = 0.0
+        else:
+            t = step / N
+        
+        vx = round(x0 + t * dx)
+        vy = round(y0 + t * dy)
+        
+        points.append(vx)
+        points.append(vy)
+        
+
+    chArray = list(gmap)
+    
+    for k in range(abs(tly - bry)):
+        for l in range(abs(tlx - brx)*2):
+            if l == 0 or l == abs(tlx - brx)*2 - 1:
+                chArray[((tly + k)*gwidth + l + tlx*2 + 1)] = char
+            elif k == abs(tly - bry) - 1:
+                chArray[((tly + k)*gwidth + l + tlx*2 + 1)] = char
+            elif k == 0:
+                chArray[((tly + k)*gwidth + l + tlx*2 + 1)] = char
+
+            #await ctx.channel.send(str((tly + k)*gwidth) + " " + str(l) + " " + str(tlx))
+    
+    gmap = "".join(chArray)
+    
+    await ctx.channel.send("```" + gmap + "```")
+
+
+>>>>>>> Stashed changes
+@bot.command()
 async def repeat(ctx, arg):
 	await ctx.channel.send(arg)
->>>>>>> 1d6e41f2f58981731df895f513a31f2baff5994c
+
+=======
+
+@bot.command()
+async def repeat(ctx, arg):
+	await ctx.channel.send(arg)
 bot.run(TOKEN)
+>>>>>>> dc4b8edf73eb58e7cbeeae07c7bfea1eaf6ad036
